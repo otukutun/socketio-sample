f6fab61485ada85402c3bcea80e6f992db884205
otukutun <orehaorz@gmail.com>
2016-12-04 16:10:17 +0900

    broadcastしたり、toしたり.

--- app.js
+++ app.js
@@ -12,4 +12,15 @@ io.sockets.on('connection', function(socket) {
   socket.on('client_to_server', function(data) {
     io.sockets.emit('server_to_client', { value : data.value });
   });
+
+  socket.on('client_to_server_broadcast', function(data) {
+    socket.broadcast.emit('server_to_client', {value: data.value});          
+  });
+
+  socket.on('client_to_server_personal', function(data) {
+    var id = socket.id;
+    name = data.value;
+    var personalMessage = "あなたは" + name + "さんとして入室しました。";
+    io.to(id).emit('server_to_client', {value: personalMessage });
+  });
 });
